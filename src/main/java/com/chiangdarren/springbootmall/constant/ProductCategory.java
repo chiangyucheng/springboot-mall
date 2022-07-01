c502a00fb454622d07c8a1ef432c22fdb444be39
Darren <Darren_Jiang@provisiontw.onmicrosoft.com>
2022-07-01 15:41:55 +0800

    商品功能-Refactor category,使用Enum類型

---
+++ src/main/java/com/chiangdarren/springbootmall/constant/ProductCategory.java
@@ -0,0 +1,9 @@
+package com.chiangdarren.springbootmall.constant;
+
+
+public enum ProductCategory {
+
+    FOOD,
+    CAR,
+    E_BOOK
+}
--- src/main/java/com/chiangdarren/springbootmall/model/Product.java
+++ src/main/java/com/chiangdarren/springbootmall/model/Product.java
@@ -1,5 +1,7 @@
 package com.chiangdarren.springbootmall.model;
 
+import com.chiangdarren.springbootmall.constant.ProductCategory;
+
 import java.util.Date;
 
 
@@ -7,7 +9,7 @@ public class Product {
 
     private Integer productId;
     private String productName;
-    private String category;
+    private ProductCategory category;
     private String imageUrl;
     private Integer price;
     private Integer stock;
@@ -32,11 +34,11 @@ public class Product {
         this.productName = productName;
     }
 
-    public String getCategory() {
+    public ProductCategory getCategory() {
         return category;
     }
 
-    public void setCategory(String category) {
+    public void setCategory(ProductCategory category) {
         this.category = category;
     }
 
